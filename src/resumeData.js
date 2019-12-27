e55ff607f481a0521c01456c54fc5af59b7d2645
Talha <talhabintariq@gmail.com>
2019-12-26 22:06:00 -0500

    updated resume date

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,103 +1,105 @@
-let resumeData = {
-    "imagebaseurl":"",
-    "name": "Talha B. Tariq",
-    "role": "I build things for the web.",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I'm a software engineer based in Ann Arbor, MI specializing in building (and occasionally designing) exceptional, high-quality websites and applications.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/ttariq1/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/talhabintariq",
-          "className":"fa fa-github"
-        }
-      ],
-    "aboutme":"I'm a software engineer based in Ann Arbor, MI specializing in building (and occasionally designing) exceptional, high-quality websites and applications.",
-    "email": 'talhabintariq@gmail.com',
-    "github": "https://github.com/talhabintariq",
-    "website": "https://www.talhabintariq.com",
-    "githubPage":"https://talhabintariq.github.io",
-    "education":[
-      {
-        "UniversityName":"GCU",
-        "specialization":"Computer Science",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2012",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+const resumeData = {
+  imagebaseurl: '',
+  name: 'Talha B. Tariq',
+  role: 'I build things for the web.',
+  linkedinId: 'Your LinkedIn Id',
+  skypeid: 'Your skypeid',
+  roleDescription:
+    "I'm a software engineer based in Ann Arbor, MI specializing in building (and occasionally designing) exceptional, high-quality websites and applications.",
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/ttariq1/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/talhabintariq',
+      className: 'fa fa-github',
+    },
+  ],
+  aboutme:
+    "I'm a software engineer based in Ann Arbor, MI specializing in building (and occasionally designing) exceptional, high-quality websites and applications.",
+  email: 'talhabintariq@gmail.com',
+  github: 'https://github.com/talhabintariq',
+  website: 'https://www.talhabintariq.com',
+  githubPage: 'https://talhabintariq.github.io',
+  education: [
+    {
+      UniversityName: 'GCU',
+      specialization: 'Computer Science',
+      MonthOfPassing: 'Aug',
+      YearOfPassing: '2012',
+      Achievements: 'Some Achievements',
+    },
+    {
+      UniversityName: 'Some University',
+      specialization: 'Some specialization',
+      MonthOfPassing: 'Jan',
+      YearOfPassing: '2018',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'Some Company',
+      specialization: 'Some specialization',
+      MonthOfLeaving: 'Jan',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements',
+    },
+    {
+      CompanyName: 'Some Company',
+      specialization: 'Some specialization',
+      MonthOfLeaving: 'Jan',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5',
+    },
+    {
+      skillname: 'CSS',
+    },
+    {
+      skillname: 'Reactjs',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'project1',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg',
+    },
+    {
+      name: 'project2',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project.jpg',
+    },
+    {
+      name: 'project3',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project2.png',
+    },
+    {
+      name: 'project4',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg',
+    },
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+  ],
+};
+
+export default resumeData;
